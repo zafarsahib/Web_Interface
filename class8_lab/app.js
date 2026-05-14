2f36b5b77cfdb44433e923d354b5f9bc1ef96faa
Muhammad Zafar <85223983+zafarsahib@users.noreply.github.com>
2026-05-14 17:43:54 -0400

    Added alert

--- class8_lab/app.js
+++ class8_lab/app.js
@@ -3,5 +3,13 @@ const clearBtn=document.getElementById("clearBtn");
 const status=document.getElementById("status");
 const usersContainer=document.getElementById("usersContainer");
 
-function setStatus(message,type){
+function setStatus(message, type) {
+
+    status.innerHTML = `
+        <div class="alert alert-${type}">
+            ${message}
+        </div>
+    `;
 }
+
+setStatus("Ready","success");
