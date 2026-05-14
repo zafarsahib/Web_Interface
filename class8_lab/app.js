a1ec148d8a8a73448985874768a2a54d50bed842
Muhammad Zafar <85223983+zafarsahib@users.noreply.github.com>
2026-05-14 18:04:11 -0400

    Added First 5 user cards function

--- class8_lab/app.js
+++ class8_lab/app.js
@@ -30,7 +30,9 @@ function loadUsers() {
 
         .then(users => {
 
-            console.log(users);
+            users
+                .slice(0,5)
+                .forEach(renderUserCard);
 
             setStatus(
                 "Users loaded successfully",
@@ -51,3 +53,47 @@ function loadUsers() {
 }
 
 loadBtn.addEventListener("click", loadUsers);
+
+function renderUserCard(user){
+
+    usersContainer.innerHTML += `
+
+    <div class="col-md-6 col-lg-4">
+
+        <div class="card h-100 shadow">
+
+            <div class="card-body">
+
+                <h5 class="card-title">
+                    ${user.name}
+                </h5>
+
+                <p>
+                    <strong>Email:</strong>
+                    ${user.email}
+                </p>
+
+                <p>
+                    <strong>Phone:</strong>
+                    ${user.phone}
+                </p>
+
+                <p>
+                    <strong>City:</strong>
+                    ${user.address.city}
+                </p>
+
+                <p>
+                    <strong>Company:</strong>
+                    ${user.company.name}
+                </p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    `;
+}
+
