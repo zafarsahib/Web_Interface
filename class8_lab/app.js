633293e771f0d7b02abb4d069be758805c1c4662
Muhammad Zafar <85223983+zafarsahib@users.noreply.github.com>
2026-05-14 17:31:19 -0400

    Added DOM selections

---
+++ class8_lab/app.js
@@ -0,0 +1,7 @@
+const loadBtn=document.getElementById("loadBtn");
+const clearBtn=document.getElementById("clearBtn");
+const status=document.getElementById("status");
+const usersContainer=document.getElementById("usersContainer");
+
+function setStatus(message,type){
+}
