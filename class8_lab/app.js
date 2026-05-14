15d470092422294e938f86cc11a6c44f35ce261e
Muhammad Zafar <85223983+zafarsahib@users.noreply.github.com>
2026-05-14 17:56:38 -0400

    Added status update

--- class8_lab/app.js
+++ class8_lab/app.js
@@ -3,6 +3,15 @@ const clearBtn=document.getElementById("clearBtn");
 const status=document.getElementById("status");
 const usersContainer=document.getElementById("usersContainer");
 
+function setStatus(message, type) {
+
+    status.innerHTML = `
+        <div class="alert alert-${type}">
+            ${message}
+        </div>
+    `;
+}
+
 function loadUsers() {
 
     setStatus("Loading users...", "warning");
