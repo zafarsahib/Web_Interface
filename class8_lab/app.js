1215c87fc1b7dee65f25653f0f546ec1818c5694
Muhammad Zafar <85223983+zafarsahib@users.noreply.github.com>
2026-05-14 17:53:02 -0400

    Added responses and error feature in load users function

--- class8_lab/app.js
+++ class8_lab/app.js
@@ -7,6 +7,38 @@ function loadUsers() {
 
     setStatus("Loading users...", "warning");
 
+    fetch("https://jsonplaceholder.typicode.com/users")
+
+        .then(response => {
+
+            if (!response.ok) {
+                throw new Error("Unable to load users");
+            }
+
+            return response.json();
+
+        })
+
+        .then(users => {
+
+            console.log(users);
+
+            setStatus(
+                "Users loaded successfully",
+                "success"
+            );
+
+        })
+
+        .catch(error => {
+
+            setStatus(
+                error.message,
+                "There was an error loading users"
+            );
+
+        });
+
 }
 
 loadBtn.addEventListener("click", loadUsers);
