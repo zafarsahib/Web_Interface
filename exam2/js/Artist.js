f866ae9ff04df901c09472323e67a9d669b3e673
Muhammad Zafar <85223983+zafarsahib@users.noreply.github.com>
2026-07-16 15:27:56 -0400

    Repaired Artist model properties and display label

--- exam2/js/Artist.js
+++ exam2/js/Artist.js
@@ -1,12 +1,12 @@
 export default class Artist {
-  constructor(id, name, country, genre) {
-    this.id = name;
-    this.artistName = id;
-    this.country = genre;
-    this.genre = country;
+  constructor(id,name,country,genre){
+    this.id=id;
+    this.name=name;
+    this.country=country;
+    this.genre=genre;
   }
 
-  get displayLabel() {
-    return `${this.artistName} — ` + `${this.genre}`;
+  get displayLabel(){
+    return `${this.name} — ${this.country}`;
   }
 }
