5d1f75b0e3cef9e5ce0941a9c66460116544f1b7
Muhammad Zafar <85223983+zafarsahib@users.noreply.github.com>
2026-07-23 12:42:08 -0400

    Created CurrentWeather custom element with Shadow DOM

--- Lea_weather_app/js/CurrentWeather.js
+++ Lea_weather_app/js/CurrentWeather.js
@@ -4,6 +4,55 @@ export class CurrentWeather extends HTMLElement {
 
         super();
 
+        this.attachShadow({
+            mode: "open"
+        });
+
+        const template =
+            document.getElementById(
+                "weather-card-template"
+            );
+
+        const clone =
+            template.content.cloneNode(true);
+
+        this.shadowRoot.appendChild(clone);
+
+    }
+
+    connectedCallback() {
+
+        this.render();
+
+    }
+
+    render() {
+
+        this.shadowRoot
+            .querySelector(".temperature")
+            .textContent =
+                "Temperature: -- °C";
+
+        this.shadowRoot
+            .querySelector(".windspeed")
+            .textContent =
+                "Wind Speed: -- km/h";
+
+        this.shadowRoot
+            .querySelector(".winddirection")
+            .textContent =
+                "Wind Direction: --°";
+
+        this.shadowRoot
+            .querySelector(".weathercode")
+            .textContent =
+                "Weather Code: --";
+
+        this.shadowRoot
+            .querySelector(".status")
+            .textContent =
+                "Waiting to load weather...";
+
     }
 
 }
